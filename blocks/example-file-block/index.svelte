de666265b3d07c479f72364422d3d63c4ffb2383
Jake Donham <jaked@github.com>
2023-01-04 14:47:53 -0800

    copied from blocks-template-svelte

---
+++ blocks/example-file-block/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { FileBlockProps } from '@githubnext/blocks';
+  export let { content } = $$props as FileBlockProps;
+</script>
+
+<h1>Hello from Svelte!</h1>
+<pre>
+{content}
+</pre>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
